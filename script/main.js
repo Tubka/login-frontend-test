f806f3f215ff31cc238a188366935cd7a5c218fc
Tubka <Tubka94@users.noreply.github.com>
2019-06-30 21:27:20 +0200

    test

--- script/main.js
+++ script/main.js
@@ -2,11 +2,8 @@ const btn = document.querySelector(".login__btn");
 const register = document.querySelector(".register");
 const form = document.querySelector(".login__form");
 
-btn.addEventListener(
-  "click",
-  (clear = e => {
-    e.preventDefault();
-    register.classList.add("clear");
-    form.classList.add("clear");
-  })
-);
+btn.addEventListener("click", e => {
+  e.preventDefault();
+  register.classList.add("clear");
+  form.classList.add("clear");
+});
